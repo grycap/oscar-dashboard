bc78db897b076744786a1a795c5d4349f95f4379
prosfus <paurosfusftm@gmail.com>
2024-09-08 18:28:38 +0200

    feat: ToU and privacy policy pages

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,6 +3,7 @@ import SidebarRouteItem from "./components/SidebarRotueItem";
 import { Codesandbox, Database, LogOut } from "lucide-react";
 import OscarColors, { OscarStyles } from "@/styles";
 import { useAuth } from "@/contexts/AuthContext";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   const authContext = useAuth();
@@ -48,20 +49,48 @@ function Sidebar() {
       </ul>
       <div
         style={{
-          marginTop: "auto",
-          height: "50px",
-          borderTop: OscarStyles.border,
+          marginTop: "auto  ",
           width: "100%",
           display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: "0 16px",
-          gap: "16px",
-          cursor: "pointer",
+          flexDirection: "column",
         }}
-        onClick={handleLogout}
       >
-        Log out <LogOut color={OscarColors.Red} />
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            padding: "6px 16px",
+          }}
+        >
+          <Link to={"/terms-of-use"}>
+            <span style={{ fontSize: "10px", color: OscarColors.DarkGrayText }}>
+              Terms of use
+            </span>
+          </Link>
+          <Link to={"/privacy-policy"}>
+            <span style={{ fontSize: "10px", color: OscarColors.DarkGrayText }}>
+              Privacy policy
+            </span>
+          </Link>
+        </div>
+        <div
+          style={{
+            height: "50px",
+            borderTop: OscarStyles.border,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "0 16px",
+            gap: "16px",
+            cursor: "pointer",
+          }}
+          onClick={handleLogout}
+        >
+          Log out <LogOut color={OscarColors.Red} />
+        </div>
       </div>
     </section>
   );
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,6 +3,8 @@ import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 import AppLayout from "@/pages/ui/layout";
 import ServicesRouter from "@/pages/ui/services/router";
 import Login from "@/pages/Login";
+import { PrivacyPolicy } from "@/pages/PrivacyPolicy";
+import TermsOfUse from "@/pages/TermsOfUse";
 
 function AppRouter() {
   return (
@@ -22,6 +24,8 @@ function AppRouter() {
         </Route>
 
         <Route path="/login" element={<Login />} />
+        <Route path="/terms-of-use" element={<TermsOfUse />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="*" element={<Navigate to="/ui/services" replace />} />
       </Routes>
     </BrowserRouter>
