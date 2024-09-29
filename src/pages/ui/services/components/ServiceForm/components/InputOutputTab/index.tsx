90d08e930f4d7fccb884628805e3c5fc164dbfe5
prosfus <paurosfusftm@gmail.com>
2024-09-29 11:09:10 +0200

    chore: remove unused imports

--- src/pages/ui/services/components/ServiceForm/components/InputOutputTab/index.tsx
+++ src/pages/ui/services/components/ServiceForm/components/InputOutputTab/index.tsx
@@ -3,7 +3,6 @@ import { PlusCircle, Pencil, Trash2, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Dialog,
   DialogContent,
