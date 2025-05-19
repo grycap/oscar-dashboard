a1b977899ac9e228e9063e0787ead81ac1414711
SergioLangaritaBenitez <sergiolangaritabenitez@gmail.com>
2025-05-19 09:11:46 +0200

    check error

--- src/pages/ui/services/components/FDL/index.tsx
+++ src/pages/ui/services/components/FDL/index.tsx
@@ -70,12 +70,12 @@ function FDLForm() {
 
     results.forEach((result, index) => {
       if (result.status === "rejected") {
+        console.log(result.reason)
+        console.log(result.status)
         if (result.reason.status === 403 &&
           result.reason.response.data.includes("doesn't have permision to modify this service")){
             console.log("Service already exist and you don't have permision")
         }
-        console.log(result.reason)
-        console.log(result.status)
         alert.error(
           `Error creating service ${services[index].name}: ${result.reason}`
         );
