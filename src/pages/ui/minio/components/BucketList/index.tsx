fb39c965cb6435a2506e49e620367b26e9f8423a
Robert Kazaryan <kazaryanrobert@gmail.com>
2025-10-23 14:59:47 +0200

    feat: enhance BucketList component with owner and service details

--- src/pages/ui/minio/components/BucketList/index.tsx
+++ src/pages/ui/minio/components/BucketList/index.tsx
@@ -2,14 +2,39 @@ import DeleteDialog from "@/components/DeleteDialog";
 import GenericTable from "@/components/Table";
 import { Button } from "@/components/ui/button";
 import { useMinio } from "@/contexts/Minio/MinioContext";
+import { alert } from "@/lib/alert";
 import OscarColors from "@/styles";
 import { Bucket } from "@aws-sdk/client-s3";
-import { LoaderPinwheel, Trash } from "lucide-react";
-import { useState } from "react";
+import { Copy, ExternalLinkIcon, LoaderPinwheel, Trash } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+interface BucketList extends Bucket {
+  from_service: string;
+  owner: string;
+}
+
 export default function BucketList() {
-  const { buckets, bucketsAreLoading, deleteBucket } = useMinio();
+  const { buckets, bucketsOSCAR, bucketsAreLoading, deleteBucket } = useMinio();
   const [itemsToDelete, setItemsToDelete] = useState<Bucket[]>([]);
+  const [bucketsList, setBucketsList] = useState<BucketList[]>([]);
+
+  useEffect(() => {
+    if (buckets) {
+      const updatedBucketsList = buckets.map((bucket) => {
+        const oscarBucket = bucketsOSCAR.find((b) => b.bucket_name === bucket.Name);
+        console.log("Mapping bucket:", bucket.Name, "to service:", oscarBucket?.bucket_name);
+        console.log("oscarBucket metadata:", oscarBucket?.metadata);
+        return {
+          ...bucket,
+          from_service: oscarBucket?.metadata?.from_service ?? "",
+          owner: oscarBucket?.owner === "" ? "oscar" : oscarBucket?.owner,
+        } as BucketList;
+      });
+      setBucketsList(updatedBucketsList);
+    }
+  }, [buckets]);
+
   return (
     <>
       <DeleteDialog
@@ -25,8 +50,8 @@ export default function BucketList() {
         <LoaderPinwheel className="animate-spin" size={60} color={OscarColors.Green3} />
       </div>
       : 
-      <GenericTable<Bucket>
-        data={buckets}
+      <GenericTable<BucketList>
+        data={bucketsList}
         columns={[
           {
             header: "Name",
@@ -34,6 +59,42 @@ export default function BucketList() {
               <Link to={`/ui/minio/${row.Name}`}>{row.Name}</Link>
             ),
             sortBy: "Name"
+          },
+          {
+            header: "Owner",
+            accessor: (row) => (
+              <div 
+                className="grid grid-cols-[auto_1fr] no-underline hover:underline underline-offset-2 cursor-pointer"
+                onClick={() => {navigator.clipboard.writeText(row.owner ? row.owner : "oscar");alert.success("Owner copied to clipboard");}}
+              >
+                <span className="truncate min-w-[40px] max-w-[70px]">
+                  {row.owner}
+                </span>
+                <Copy size={12} className="self-center ml-[2px]" />
+              </div>
+            ),
+            sortBy: "owner"
+          },
+          {
+            header: "Service",
+            accessor: (row) => (
+              <>
+                {row.from_service ?
+                  <Link
+                    to={`/ui/services/${row.from_service}/settings`}
+                    className="grid grid-cols-[auto_1fr] no-underline hover:underline underline-offset-2 border-gray-400"
+                  >
+                    <span className="truncate min-w-[70px] max-w-[70px]">
+                      {row.from_service}
+                    </span>
+                    <ExternalLinkIcon size={12} className="self-center ml-[2px]"/>
+                  </Link>
+                  :
+                  <span></span>
+                }
+              </>
+            ),
+            sortBy: "from_service"
           },
         ]}
         actions={[
