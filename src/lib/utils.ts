573b94ab9457d6495e27e90092bcea880df863fb
Robert Kazaryan <kazaryanrobert@gmail.com>
2025-11-18 16:57:48 +0100

    feat: add function to convert byte size to human-readable format

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -131,3 +131,12 @@ export function isSafariBrowser(): boolean {
   const isSafari = /^((?!chrome|android).)*safari/i.test(ua) && provider.includes("Apple");
   return isSafari;
 }
+
+export function bytesSizeToHumanReadable(size: number): string {
+  if (size === 0) return '0 Bytes';
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(size) / Math.log(k));
+  const humanReadableSize = parseFloat((size / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+  return humanReadableSize;
+}
