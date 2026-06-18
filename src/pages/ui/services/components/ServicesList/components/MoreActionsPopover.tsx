6afa31c27b5bf2f2770a214ad673e7f2878f67ac
Robert Kazaryan <kazaryanrobert@gmail.com>
2026-06-18 12:02:24 +0200

    fix script download name in more action popover

--- src/pages/ui/services/components/ServicesList/components/MoreActionsPopover.tsx
+++ src/pages/ui/services/components/ServicesList/components/MoreActionsPopover.tsx
@@ -41,7 +41,7 @@ export default function MoreActionsPopover({
     
     const { fdlText, scriptText } = getFDLAndScriptText(service);
     const content = type === "fdl" ? fdlText : scriptText;
-    downloadString(content, `${service.name}.${type === "fdl" ? "yaml" : "sh"}`);
+    downloadString(content, `${service.name}${type === "fdl" ? ".yaml" : "-script.sh"}`);
   }
 
   return (
