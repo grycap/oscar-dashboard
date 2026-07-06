e58ece9c7d123956bff31e2ff478325461124474
Robert Kazaryan <kazaryanrobert@gmail.com>
2026-07-06 15:00:37 +0200

    feat: integrate StorageSelectForm for bucket and volume selection in JunoFormPopover

--- src/pages/ui/juno/components/JunoFormPopover/index.tsx
+++ src/pages/ui/juno/components/JunoFormPopover/index.tsx
@@ -6,7 +6,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useAuth } from "@/contexts/AuthContext";
-import useGetPrivateBuckets from "@/hooks/useGetPrivateBuckets";
 import { alert } from "@/lib/alert";
 import { convertDockerImageToMap, fetchFromGitHubOptions, generateReadableName, genRandomString, getAllowedVOs, isVersionLower } from "@/lib/utils";
 import yamlToServices from "@/pages/ui/services/components/FDL/utils/yamlToService";
@@ -19,13 +18,12 @@ import { IMAGE_TAGS } from "./images";
 import InfoPopUp from "@/components/InfoPopUp";
 import InfoItem from "@/pages/ui/info/components/InfoItem";
 import { errorMessage } from "@/lib/error";
+import StorageSelectForm from "@/components/StorageSeceltForm";
 
 function JunoFormPopover() {
   const [isOpen, setIsOpen] = useState(false);
   const {systemConfig, authData, clusterInfo } = useAuth();
   const { refreshServices } = useServicesContext();
-  const [newBucket, setNewBucket] = useState(false);
-  const buckets = useGetPrivateBuckets();
 
   const oidcGroups = getAllowedVOs(systemConfig, authData);
   const imageTagsMap = convertDockerImageToMap(IMAGE_TAGS);
@@ -40,6 +38,9 @@ function JunoFormPopover() {
     memoryRam: "2",
     memoryUnit: "Gi",
     bucket: "",
+    volume: "",
+    volumeSize: "",
+    mainStorage: "bucket",
     vo: "",
     token: "",
     imageTag: IMAGE_TAGS[0].tag,
@@ -50,6 +51,8 @@ function JunoFormPopover() {
     cpuCores: false,
     memoryRam: false,
     bucket: false,
+    volume: false,
+    volumeSize: false,
     vo: false,
     token: false,
   });
@@ -69,6 +72,9 @@ function JunoFormPopover() {
       memoryRam: "2",
       memoryUnit: "Gi",
       bucket: "",
+      volume: "",
+      volumeSize: "",
+      mainStorage: "bucket",
       token: genRandomString(128),
       imageTag: IMAGE_TAGS[0].tag,
     }));
@@ -79,6 +85,8 @@ function JunoFormPopover() {
       bucket: false,
       vo: false,
       token: false,
+      volume: false,
+      volumeSize: false,
     });
   }, [isOpen]);
 
@@ -87,9 +95,11 @@ function JunoFormPopover() {
       name: !formData.name,
       cpuCores: !formData.cpuCores,
       memoryRam: !formData.memoryRam,
-      bucket: !formData.bucket,
+      bucket: formData.mainStorage === "bucket" && !formData.bucket,
       vo: !formData.vo,
       token: !formData.token,
+      volume: formData.mainStorage === "volume" && !formData.volume,
+      volumeSize: errors.volumeSize,
     };
 
     setErrors(newErrors);
@@ -116,6 +126,12 @@ function JunoFormPopover() {
 
       const serviceName = formData.name || nameService();
 
+      const workspaceDir = formData.mainStorage === "volume" && formData.volume
+        ? `/mnt/volumes/${formData.volume}`
+        : formData.mainStorage === "bucket" && formData.bucket
+          ? `/mnt/${formData.bucket}`
+          : `/tmp/${serviceName}`;
+
       const modifiedService: Service = {
         ...service,
         image: `${service.image.split(':')[0]}:${formData.imageTag}`,
@@ -123,16 +139,11 @@ function JunoFormPopover() {
         vo: formData.vo,
         memory: `${formData.memoryRam}${formData.memoryUnit}`,
         cpu: formData.cpuCores,
-        mount: {
-          ...service.mount,
-          path: formData.bucket ?? "/notebook",
-          storage_provider: service.mount?.storage_provider ?? "minio.default",
-        },
         environment: {
           variables: {
             ...service.environment.variables,
             JHUB_BASE_URL: `/system/services/${serviceName}/exposed`,
-            JUPYTER_DIRECTORY: "/mnt/"+ formData.bucket,
+            JUPYTER_DIRECTORY: workspaceDir,
             GRANT_SUDO: "yes",
             OSCAR_ENDPOINT: authData.endpoint,
             JUPYTER_TOKEN: formData.token,
@@ -145,6 +156,21 @@ function JunoFormPopover() {
           ...service.labels,
           jupyter_notebook: "true",
         },
+        mount: undefined,
+        ...(formData.bucket ? {
+          mount: {
+            ...service.mount,
+            path: formData.bucket ?? "/notebook",
+            storage_provider: service.mount?.storage_provider ?? "minio.default",
+          },
+        } : {}),
+        ...(formData.volume ? { 
+          volume: {
+            name: formData.volume,
+            size: formData.volumeSize ? `${formData.volumeSize.trim()}Gi` : undefined,
+            mount_path: `/mnt/volumes/${formData.volume}`,
+          }
+        } : {}),
       };
       await createServiceApi(modifiedService);
       refreshServices();
@@ -347,51 +373,12 @@ return (
               ></Input>
             </div>
             <div>
-              <Label>Bucket</Label>
-              <hr className="mb-2"/>
-              <div>
-                <Label className="inline-flex items-center cursor-pointer">
-                  <input type="checkbox" value="" className="sr-only peer" onClick={() => { setNewBucket(!newBucket); setFormData({ ...formData, bucket: "" }); }} />
-                  <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600 dark:peer-checked:bg-teal-600"></div>
-                  <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">New Bucket</span>
-                </Label>
-                {newBucket? 
-                <Input
-                  type="input"
-                  onFocus={(e) => (e.target.type = "text")}
-                  style={{ width: "100%",
-                    fontWeight: "normal",
-                    }}
-                  className={errors.bucket ? "border-red-500 focus:border-red-500" : ""}
-                  onChange={(e) => {
-                      setFormData({ ...formData, bucket: e.target?.value });
-                      if (errors.bucket) setErrors({ ...errors, bucket: false });
-                  }}
-                  placeholder="Enter new bucket name"
-                />
-                :
-                  <Select
-                    value={formData.bucket}
-                    onValueChange={(value) => {
-                      setFormData({ ...formData, bucket: value });
-                      if (errors.bucket) setErrors({ ...errors, bucket: false });
-                    }}
-                  >
-                    <SelectTrigger className={errors.bucket ? "border-red-500 focus:border-red-500" : ""}>
-                      <SelectValue
-                        placeholder="Select a bucket"
-                      />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {buckets.map((bucket) => (
-                        <SelectItem key={bucket.bucket_name} value={bucket.bucket_name}>
-                          {bucket.bucket_name}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                }
-              </div>
+              <StorageSelectForm 
+                formData={formData}
+                setFormData={setFormData}
+                errors={errors}
+                setErrors={setErrors}
+              />
             </div>
           </div>
         <DialogFooter>
