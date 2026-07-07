b20569cc00faefce820b4b2653657d003ab1c2fd
Robert Kazaryan <kazaryanrobert@gmail.com>
2026-07-07 12:08:09 +0200

    feat: add error styling to Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -41,7 +41,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           className={cn(
             "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:bg-white",
             className,
-            (endIcon || type === "password") && "pr-10"
+            (endIcon || type === "password") && "pr-10",
+            props.error && "border-red-500 focus:border-red-500"
           )}
           ref={ref}
           {...props}
