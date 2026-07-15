c280a70802d962a6a4fa4e158ec8d335f9814a43
Robert Kazaryan <kazaryanrobert@gmail.com>
2026-07-15 12:50:26 +0200

    feat: add support for RO-Crate Agent service definitions and enhance parsing logic

--- src/lib/roCrate.d.ts
+++ src/lib/roCrate.d.ts
@@ -12,9 +12,34 @@ export interface RoCrateServiceDefinition {
     gpuRequirements: string;
 }
 
+export interface RoCrateAgentServiceDefinition {
+    name: string;
+    description: string;
+    fdlUrl: string;
+    scriptUrl: string;
+    author: string;
+    type: Array<string>;
+    iconUrl: string;
+    memoryRequirements: string;
+    memoryUnits: string;
+    cpuRequirements: string;
+    gpuRequirements: string;
+    agentSoulUrl: string;
+    agentSkillUrl: Array<string>;
+    agentType: "exposed" | "on-demand",
+}
+
 /**
  * Parses the RO-Crate data and returns an array of service definitions.
  * @returns {Promise<RoCrateServiceDefinition[]>} A promise that resolves to an array of service definitions.
  */
 declare const parseROCrateDataJS: (githubUser: string, githubRepo: string, githubBranch: string, validate = false) => Promise<RoCrateServiceDefinition[]>;
-export default parseROCrateDataJS;
+
+/**
+ * Parses the OSCAR Agents RO-Crate data and returns an array of service definitions.
+ * @returns {Promise<RoCrateAgentServiceDefinition[]>} A promise that resolves to an array of service definitions.
+ */
+declare const parseAgentsROCrateDataJS: (githubUser: string, githubRepo: string, githubBranch: string, validate = false) => Promise<RoCrateAgentServiceDefinition[]>;
+
+//export default parseROCrateDataJS;
+export { parseROCrateDataJS, parseAgentsROCrateDataJS };
